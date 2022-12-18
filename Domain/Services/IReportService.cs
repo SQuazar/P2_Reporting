6e10fdcad59328f447595cab0606661e83ba1056
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-18 03:10:35 +0300

    Added new method for query optimization

--- Domain/Services/IReportService.cs
+++ Domain/Services/IReportService.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain.Models;
 
@@ -7,4 +8,5 @@ namespace Domain.Services;
 public interface IReportService : IDataService<Report>, IPaginatedService<Report>
 {
     Task<Report> Send(Account sender, string title, string? description = null);
+    Task<IEnumerable<Report>> GetAllBySender(Account sender);
 }
